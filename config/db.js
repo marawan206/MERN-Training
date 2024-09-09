224041bdd90590a8bb7d02003f4cb2a341cfa9c4
Marwan Ahmed <155799754+marawan206@users.noreply.github.com>
2024-09-09 16:09:09 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mogoURI');
+const db = config.get('mongoURI'); // Corrected typo
+// No need to import these since they are already imported in the server.js file
 
-const connectDB = async () => {
+async function connectDB () {
     try {
-        await mongoose.connect(db);
+        await mongoose.connect(db, { // Could use .env for your mongouri and .gitignore it
+            useNewUrlParser: true, 
+            useUnifiedTopology: true, // Options to prevent deprecation warnings
+        });
 
         console.log('MongoDB Connected!');
-    } catch(err) {
+    } catch (err) {
         console.log(err.message);
-        //Exit process with failure
+        // Exit process with failure
         process.exit(1);
     }
 }
